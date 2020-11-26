8bdc0a67b60d302db4cb8d780d9442fff721581a
anandshivam44 <anand.shivam44@yahoo.com>
2020-11-26 17:47:28 +0530

    improved nav bar

--- src/compoments/Card/Card.js
+++ src/compoments/Card/Card.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button,Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 
 const CardMod = () => {
     return (
@@ -15,8 +15,7 @@ const CardMod = () => {
                     <Card.Title>Card Title</Card.Title>
                     <Card.Text>
                         Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-    </Card.Text>
+                        the card's content.</Card.Text>
                     <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
             </Card>
