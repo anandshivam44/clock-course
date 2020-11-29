779a7d6fdcca7d5074068c35edc3e8cf5e10f740
Aman Tiwari <amantiwari@78632.com>
2020-11-29 19:32:33 +0530

    improved the nav code and remove css code

--- src/compoments/NavBar/NavBar.js
+++ src/compoments/NavBar/NavBar.js
@@ -2,18 +2,44 @@ import React from "react";
 import { Navbar, Nav, Form, FormControl, Button,Container } from "react-bootstrap";
 
 const NavbarMod = () => {
+
+  const NavName = [
+    {
+      name:"Home",
+      href:"#"
+    },
+    {
+      name:"Courses",
+      href:"#"
+    },
+    {
+      name:"Blogs",
+      href:"#"
+    },
+    {
+      name:"Test Series",
+      href:"#"
+    },
+    {
+      name:"Interview Questions",
+      href:"#"
+    },
+    {
+      name:"Contact Us",
+      href:"#"
+    },
+  ]
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#">Cock Course</Navbar.Brand>
           <Nav className="mr-auto">
-            <Nav.Link href="#">Home</Nav.Link>
-            <Nav.Link href="#">Courses</Nav.Link>
-            <Nav.Link href="#">Blogs</Nav.Link>
-            <Nav.Link href="#">Test Series</Nav.Link>
-            <Nav.Link href="#">Interview Questions</Nav.Link>
-            <Nav.Link href="#">Contact Us</Nav.Link>
+            {
+              NavName.map(data => (
+              <Nav.Link href={data.href}>{data.name}</Nav.Link>
+              ))
+            }
           </Nav>
           <Form inline>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
